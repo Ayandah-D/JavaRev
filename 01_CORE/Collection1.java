432756b766c72e9887c550e62c42ea162a7a03db
Ayandah-D <87431114+Ayandah-D@users.noreply.github.com>
2025-01-15 14:39:54 +0200

    progress

---
+++ 01_CORE/Collection1.java
@@ -0,0 +1,23 @@
+
+import  java.util.ArrayList;
+import  java.util.Collection;
+
+import javax.xml.transform.Source;
+public class Collection1{
+    public static void main(String []args){
+
+        Collection <Integer>nums = new ArrayList<Integer>();
+
+        nums.add(6);
+        nums.add(3);
+        nums.add(8);
+        nums.add(5);,
+        nums.add(2);
+gfyuofyo
+        for(int n : nums ) {
+            System.out.println(n);
+        }
+    }
+
+o
+}
---
+++ 01_CORE/Stream1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import  java.util.List;
+import java.util.Array;
+public class Stream1{
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4,5,7,8,1,5,8);
+
+        int sum =0;
+         for(int n : )
+    }
+
+}
